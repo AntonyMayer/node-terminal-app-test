827d143aa6bb51b7d27e58ced9765e6a85bf063e
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-02-23 12:00:14 -0600

    defining major functions

--- index.js
+++ index.js
@@ -5,17 +5,43 @@
 const program = require('commander'),
     exec = require('child_process').exec;
 
+/**
+ * Main function, send request to Jira and triggers function to handle reresponse
+ * @param {string} projectName project name
+ * @param {string} options ticket's flag [optional]
+ */
 let get = (projectName, options) => {
-
-    let sendJiraRequest = console.log(projectName);
-
-    let execCallback = (error, stdout, stderr) => {
-        if (error) console.log("exec error: " + error);
-        if (stdout) console.log("Result: " + stdout);
-        if (stderr) console.log("shell error: " + stderr);
-    };
-    exec(sendJiraRequest, execCallback);
+    //some logic to handle user input will be there soon
+    exec(sendJiraRequest(projectName, options), execCallback);
 }
+
+/**
+ * Callback function to log errors in terminal
+ */
+let execCallback = (error, stdout, stderr) => {
+    if (error) console.log("exec error: " + error);
+    if (stdout) console.log("Result: " + stdout);
+    if (stderr) console.log("shell error: " + stderr);
+};
+
+/**
+ * Function to create and send request to Jira
+ * @param {string} projectName project name
+ * @param {string} options ticket's flag [optional]
+ */
+let sendJiraRequest = (projectName, options) => {
+    console.log("\nSending request to pull tickets for: " + projectName);
+    exec(handleJiraResponse(projectName, options), execCallback);
+}
+
+/**
+ * Function to handle request from Jira
+ * @param {string} projectName project name
+ * @param {string} options ticket's flag [optional]
+ */
+let handleJiraResponse = (projectName, options) => {
+    console.log("Tickets for " + projectName + ":");
+} 
 
 program
     .version('1.0.0')
