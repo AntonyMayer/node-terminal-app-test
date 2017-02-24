559efb0ecba3ada0b856004920b125b90c19ce38
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-02-24 14:31:35 -0600

    updated package json, waiting for jira...

--- modules/display.js
+++ modules/display.js
@@ -5,21 +5,32 @@
  */
 module.exports = (jira, data) => {
     let Table = jira.table,
-        dataTable = new Table;
+        output = new Table,
+        tableData = [];
 
-    let testData = [
-        { id: "CMCM-261", title: 'Something awesome', status: "open" },
-        { id: "CMCM-262", title: 'Very interesting ticket', status: "reopened" },
-        { id: "CMCM-263", title: 'Yet another ticket', status: "closed" }
-    ];
+    // let testData = [
+    //     { id: "CMCM-261", title: 'Something awesome', status: "open" },
+    //     { id: "CMCM-262", title: 'Very interesting ticket', status: "reopened" },
+    //     { id: "CMCM-263", title: 'Yet another ticket', status: "closed" }
+    // ];
 
-    testData.forEach(function(ticket) {
-        dataTable.cell('Ticket #', ticket.id);
-        dataTable.cell('Title', ticket.title);
-        dataTable.cell('Status', ticket.status);
-        dataTable.newRow();
+    data.response.issues.forEach((issue) => {
+        tableData.push({ 
+            id: issue.id, 
+            title: issue.fields.issuetype.description, 
+            status: issue.fields.summary,
+            url: issue.self
+        })
     });
 
-    console.log(dataTable.toString());
+    tableData.forEach(function(ticket) {
+        output.cell('Ticket #', ticket.id);
+        output.cell('Title', ticket.title);
+        output.cell('Status', ticket.status);
+        output.cell('URL', ticket.url);
+        output.newRow();
+    });
+
+    console.log(output.toString());
 
 };
