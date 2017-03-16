24cf7a3b749e90ca9459dc38c2275669d8dcfafe
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-03-16 12:52:53 -0500

    check js upd

--- modules/check.js
+++ modules/check.js
@@ -11,10 +11,9 @@ module.exports = (jira) => {
     jira.data.user = jira.store.get('username') || process.env.USER;
     jira.data.server = jira.store.get('server');
 
-    if (!jira.data.project) {
-        jira.data.project = jira.store.get('project');
-    }
-    
-    return jira;
+    if (jira.test('./.jira')) jira.data.project = JSON.parse(jira.readFile('./.jira'));
+    else if (!jira.data.project) jira.data.project = jira.store.get('project');
+
+    return jira;    
 
 };
