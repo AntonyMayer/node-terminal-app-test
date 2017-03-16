648da935dcc56080d4b48e6feed5c9229a7b6436
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-03-16 10:51:25 -0500

    config js update

--- modules/config.js
+++ modules/config.js
@@ -10,12 +10,9 @@ module.exports = (jira) => {
 
     console.log('\nChange default project...\n');
 
-    //prompt user's data
-    let userData = [{ name: 'project' }];
-
     jira.prompt.start();
 
-    jira.prompt.get(userData, function(err, results) {
+    jira.prompt.get([{ name: 'project' }], function(err, results) {
         if (err) { console.log(err); }
         jira.store.set('project', results.project)
     });
