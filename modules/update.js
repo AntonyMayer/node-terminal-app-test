04dd4948de011dea4124f09accca53093e8f12af
Anton Kuzniatsou <anton.kuzniatsou@designory.com>
2017-03-29 16:15:30 -0500

    update js

--- modules/update.js
+++ modules/update.js
@@ -2,18 +2,23 @@ module.exports = (jira) => {
     let user = jira.data.user,
         server = jira.data.server,
         pswd = jira.getPassword(jira.data.user),
+        update = JSON.stringify('{ "update": {"comment": [{"add": {"body": "Status updated"}}]},"fields": {},"transition": {"id": "221"}}'),
         queriesCounter = 0;
 
     console.log("\nWorking on update queries...\n");
 
     for (let issue of jira.data.response.issues) {
         if (issue.fields.status.name === "Dev Complete") {
-            let query = `curl -D- -u ${user}:${pswd} -X POST --data @update.json -H "Content-Type: application/json" ${server}/rest/api/2/issue/${issue.key}/transitions`;
+            let query = `curl -D- -u ${user}:${pswd} -X POST --data ${update} -H ` + 
+            `"Content-Type: application/json" ${server}/rest/api/2/issue/${issue.key}/transitions`;
             queriesCounter++;
             process.stdout.write(`Sending queries: ${queriesCounter}\r`);
             jira.curl(query);
         }
     }
-    if (queriesCounter < 1) console.log('No tickets was updated'); 
-    console.log(`\nDone updating\n`);
+    if (queriesCounter < 1) {
+        console.log('No tickets was updated\n');
+    } else {
+        console.log(`\n\nDone updating\n`);
+    }
 };
